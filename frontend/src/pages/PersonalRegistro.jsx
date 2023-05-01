0118c4609e13ef62e95ec8e06b9372a91446196a
AntonioUnssGit <AntonioUnssGit>
2023-04-30 20:06:46 -0400

    cambios de jose, agregar ruta para el formulario de agregar personal

---
+++ frontend/src/pages/PersonalRegistro.jsx
@@ -0,0 +1,9 @@
+import ButtonBoxAdmin from "../components/ButtonBoxAdmin";
+
+function PersonalRegistro(){
+    return <div className="caja">
+        <ButtonBoxAdmin/>
+        <h1>Hola como estas</h1>
+    </div>
+  }
+  export default PersonalRegistro;
