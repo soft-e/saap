e04029cc71ecf9d5a318e80974e95e44924b7db4
AntonioUnssGit <AntonioUnssGit>
2023-05-18 21:01:52 -0400

    cambios en los formularios para ver si se repiten los datos

--- frontend/src/components/componentes-jose/FormularioRegistroPersonal.jsx
+++ frontend/src/components/componentes-jose/FormularioRegistroPersonal.jsx
@@ -2,10 +2,142 @@ import '../../assets/css/css-jose/formularioRegistroPersonas.css'
 import React from 'react';
 import { Formik } from "formik";
 import axios from 'axios';
+import { useState } from 'react';
+import {link, useNavigate} from 'react-router-dom';
 
-const endPoint='http://127.0.0.1:8000/api';
+const endPoint='http://127.0.0.1:8000/api/registrarEmpleados';
+
+export const FormularioRegistroPersonas = () => {
+    const [ci, setCi] = useState('')
+    const [nombre, setNombre] = useState('')
+    const [apellido_paterno, setApellido_paterno] = useState('')
+    const [apellido_materno, setApellido_materno] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [nombre_cargo, setNombre_cargo] = useState('guardia')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    const store = async (e) => {
+        e.preventDefault()
+        await axios.post(endPoint, {
+            ci: ci,
+            nombre: nombre,
+            apellido_paterno: apellido_paterno,
+            apellido_materno: apellido_materno,
+            telefono: telefono,
+            nombre_cargo: nombre_cargo,
+            email: email,
+            password: password,
+        })
+        navigate('/')
+    }
+    
+    return(
+        <div className='cardRegistroPersonal'>
+            <form onSubmit={store}>
+                <div>
+                    <label htmlFor='ci'>Celula de Identidad</label>
+                    <input 
+                        type='text'
+                        id='ci'
+                        name='ci'
+                        placeholder='escribe tu numero de carnet'
+                        value={ci}
+                        onChange={ (e) => setCi(e.target.value) }
+                    />
+                </div>
+                <div>
+                    <label htmlFor='nombre'>nombres</label>
+                    <input 
+                        type='text'
+                        id='nombre'
+                        name='nombre'
+                        placeholder='escribe tu nombre'
+                        value={nombre}
+                        onChange={ (e) => setNombre(e.target.value) }
+                        />
+                </div>
+                <div>
+                    <label htmlFor='apellido_paterno'>Apellido Paterno</label>
+                    <input 
+                        type='text'
+                        id='apellido_paterno'    
+                        name='apellido_paterno'
+                        placeholder='escribe tu apellido Paterno'
+                        value={apellido_paterno}
+                        onChange={ (e) => setApellido_paterno(e.target.value) }
+                        />
+                </div>
+                <div>
+                    <label htmlFor='apellido_materno'>Apellido Materno</label>
+                    <input 
+                        type='text'
+                        id='apellido_materno'
+                        name='apellido_materno'
+                        placeholder='escribe tu apellido Materno'
+                        value={apellido_materno}
+                        onChange={ (e) => setApellido_materno(e.target.value) }
+                        />
+                </div>
+                <div>
+                    <label htmlFor='telefono'>Telefono</label>
+                    <input 
+                        type='text'
+                        id='telefono'
+                        name='telefono'
+                        placeholder='escribe tu numero de Telefono'
+                        value={telefono}
+                        onChange={ (e) => setTelefono(e.target.value) }
+                    />
+                </div>
+                <div>
+                    <label htmlFor='nombre_cargo'>Empleado</label>
+                    <select 
+                        type='text' 
+                        id='nombre_cargo'
+                        name="nombre_cargo"
+                    >
+                        <option value='Administrador'> Administrador </option>
+                        <option value='Guardia'> Guardia </option>
+                        <option value='Secretaria'>Secretaria</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor='email'>Correo Electronico</label>
+                    <input 
+                        type='email'
+                        id='email'
+                        name='email'
+                        placeholder='escribe tu Correo Electronio'
+                        value={email}
+                        onChange={ (e) => setEmail(e.target.value) }
+                    />
+                </div>
+                <div>
+                    <label htmlFor='password'>Contraseña</label>
+                    <input 
+                        type='password'
+                        id='password'
+                        name='password'
+                        placeholder='escribe tu contraseña aqui'
+                        value={values.password}
+                        onChange={ (e) => setPassword(e.target.value) }
+                        />
+                    </div>
+                    <div className="espacioBoton">
+                        <button  className='stylesButton' type="submit">
+                            Guardar
+                        </button>
+                    </div>
+                </form>
+        </div>
+    );
+    
+}
 
 
+/**
 export const FormularioRegistroPersonas = () => {
 
     return(
@@ -151,6 +283,7 @@ export const FormularioRegistroPersonas = () => {
                             id='nombre_cargo'
                             name="nombre_cargo"
                         >
+                            <option value='Administrador'> Administrador </option>
                             <option value='Guardia'> Guardia </option>
                             <option value='Secretaria'>Secretaria</option>
                         </select>
@@ -192,3 +325,4 @@ export const FormularioRegistroPersonas = () => {
         </div>
     );
 }
+ */
