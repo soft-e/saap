45eb2eb4aa3a8bc861cfe88bb82d21c3db770d2d
AntonioUnssGit <AntonioUnssGit>
2023-05-19 16:40:57 -0400

    subir el formulario de registrar clientes

--- frontend/src/components/componentes-jose/FormularioRegistroClientes.jsx
+++ frontend/src/components/componentes-jose/FormularioRegistroClientes.jsx
@@ -1,4 +1,4 @@
-import '../../assets/css/css-jose/formularioRegistroPersonas.css'
+gitimport '../../assets/css/css-jose/formularioRegistroPersonas.css'
 import React from 'react';
 import axios from 'axios';
 import { useState } from 'react';
