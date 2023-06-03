4204797d61c30ae8d9f867f06184ed6dc998bde3
remrodri <rembert.rodrigo@gmail.com>
2023-06-02 20:14:14 -0400

    intentando arregla

--- frontend/src/pages/pages-deysi/PlazasDisponibles.jsx
+++ frontend/src/pages/pages-deysi/PlazasDisponibles.jsx
@@ -1,31 +1,46 @@
 /* eslint-disable react/prop-types */
-
-
-
+import React from 'react';
 import '../../assets/css/css-deysi/plazasDisponibles.css';
 
-const PlazasDisponibles = ({ plazas, handleClick }) => {
-  return (
-    
-   <div>
-         <h2>Plazas Disponibles</h2>
-         
-         <div className="plazas-disponibles" >
-         
-      {plazas.map((plaza) => (
-        <div
-          key={plaza.id}
-          className="plaza-disponible"
-          onClick={() => handleClick(plaza.id)}
-        >
-         <h3 >  {plaza.id} </h3> 
-        </div>
+class PlazasDisponibles extends React.Component {
+  render() {
+    const { plazas, handleClick, bloqueSeleccionado } = this.props;
 
-          
-      ))}
-    </div>
-    </div>
-  );
-};
+    // Filtrar las plazas por bloque seleccionado
+    const plazasFiltradas =
+      bloqueSeleccionado === ''
+        ? plazas
+        : plazas.filter((plaza) => plaza.bloque === bloqueSeleccionado);
+
+    // Obtener el texto del encabezado h2 con el número de bloque
+    const encabezadoH2 =
+      bloqueSeleccionado === ''
+        ? 'Plazas Disponibles'
+        : `Plazas Disponibles (Bloque ${bloqueSeleccionado})`;
+
+    return (
+      <div>
+        <h2 className='tituloDisponible'>
+            <span className='tituloTexto'> {encabezadoH2}</span>
+        
+        </h2>
+
+      
+
+        <div className="plazas-disponibles">
+          {plazasFiltradas.map((plaza) => (
+            <div
+              key={plaza.id}
+              className="plaza-disponible"
+              onClick={() => handleClick(plaza.id)}
+            >
+              <h3>{plaza.numero}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PlazasDisponibles;
