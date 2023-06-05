57f95e52e2e807305c04651b7ae5af2763b286bd
remrodri <rembert.rodrigo@gmail.com>
2023-06-05 02:03:41 -0400

    trabajando en el login

--- frontend/src/api/atencion.api.js
+++ frontend/src/api/atencion.api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { URL_API } from "../services/EndPoint";
 
 export const getAtencionesRequest = async () =>
-  await axios.get("http://127.0.0.1:8000/api/atencion");
+  await axios.get(URL_API+"/atencion");
 export const getAtencionRequest = async (id) =>
-  await axios.get("http://localhost:8000/api/atencion/" + id)
+  await axios.get(URL_API+"/atencion/" + id)
 export const createAtencionRequest = async (atencion) =>
-  await axios.post("http://localhost:8000/api/atencion", atencion);
+  await axios.post(URL_API+"/atencion", atencion);
 export const updateAtencionRequest = async (id, newFields) =>
-  await axios.put("http://localhost:8000/api/atencion/" + id, newFields);
+  await axios.put(URL_API+"/atencion/" + id, newFields);
 export const deleteAtencionRequest = async (id) =>
-  await axios.delete("http://localhost:8000/api/atencion/" + id);
+  await axios.delete(URL_API+"/atencion/" + id);
