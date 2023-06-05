86a3a66fcc711214ef814d5dc90aa5ccb1bd6763
Baldellon <201710020@est.umss.edu>
2023-06-04 23:49:01 -0400

    Ya se tiene la vista de las quejas, recupera de la bd Y vista pag contratos

--- frontend/src/pages/pages-jhonatan/ContratosPage.jsx
+++ frontend/src/pages/pages-jhonatan/ContratosPage.jsx
@@ -3,10 +3,12 @@ import Navbar from "../../components/Navbar.jsx";
 import React, { useEffect, useState } from 'react';
 import CardCliente from "../../components/CardCliente.jsx";
 import '../../assets/css/css-jhonatan/cardCliente.css';
+import { useNavigate } from "react-router-dom";
 
 
 function ContratosPage() {
   const [docentes, setDocentes] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getDocentes = async () => {
@@ -22,7 +24,10 @@ function ContratosPage() {
       <ButtonBoxAdmin />
       <div className="contenedor_personal">
         <h1 className="title">Contratos</h1>
-        <button className="button">Registrar Contrato</button>
+        <button 
+        className="button"
+        onClick={ () => navigate("/listardocentes") }
+        >Registrar Contrato</button>
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
           {docentes.map((docente) => (
             <CardCliente key={docente.id} docente={docente} />
