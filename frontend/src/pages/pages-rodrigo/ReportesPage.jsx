87f5c5a7204be6cca1f24132974ceee7c683d0e4
remrodri <rembert.rodrigo@gmail.com>
2023-06-07 16:10:18 -0400

    implementacion del boton reportes solo interfaz

--- frontend/src/pages/pages-rodrigo/ReportesPage.jsx
+++ frontend/src/pages/pages-rodrigo/ReportesPage.jsx
@@ -1,20 +1,71 @@
 import ButtonBoxAdmin from "../../components/ButtonBoxAdmin";
 import Navbar from "../../components/Navbar";
-function ReportesPage(){
-  return(
+import "../../assets/css/css-rodrigo/ReportesPage.css"
+function ReportesPage() {
+  return (
     <>
-    <Navbar accion="cerrar sesion"/>
-    <div
-      className="espacioPagina"
-    >
-      <ButtonBoxAdmin/>
+      <Navbar accion="cerrar sesion" />
       <div
-        className="espacioDeTrabajo"
+        className="espacioPagina"
       >
-        
+        <ButtonBoxAdmin />
+        <div
+          className="espacioDeTrabajo"
+        >
+          <div
+            className="tituloPagina"
+          >
+            <h1>Reportes</h1>
+          </div>
+          <div
+            className="espacioReportes"
+          >
+            <div
+              className="espacioSelects"
+            >
+              <h2>seleccionar un mes y una semana</h2>
+              <select name="selectMonth" id="selectMonth"
+                className="selectMonth"
+              >
+                <option value="1">Enero</option>
+                <option value="2">Febrero</option>
+                <option value="3">Marzo</option>
+                <option value="4">Abril</option>
+                <option value="5">Mayo</option>
+              </select>
+              <select name="selectWeek" id="selectWeek"
+                className="selectWeek"
+              >
+                <option value="1">Semana 1</option>
+                <option value="2">Semana 2</option>
+                <option value="3">Semana 3</option>
+                <option value="4">Semana 4</option>
+              </select>
+            </div>
+            <div
+              className="espacioCardsReportes"
+            >
+              <div
+                className="cardRecaudaciones"
+              >
+                <h3>Recaudaciones</h3>
+              </div>
+              <div
+                className="cardClientesEnMora"
+              >
+                <h3>Clientes en mora</h3>
+              </div>
+              <div
+                className="cardEspaciosDisponibles"
+              >
+                <h3>Espacios disponibles</h3>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
-    </div>
-      
+
     </>
   )
 }
