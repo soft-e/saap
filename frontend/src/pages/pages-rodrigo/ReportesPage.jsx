f020f6ef15925a12e671a38a0e39b13109aabf44
remrodri <rembert.rodrigo@gmail.com>
2023-06-12 17:52:39 -0400

    pagina de reportes terminada a nivel global

--- frontend/src/pages/pages-rodrigo/ReportesPage.jsx
+++ frontend/src/pages/pages-rodrigo/ReportesPage.jsx
@@ -8,27 +8,27 @@ import { useParqueos } from "../../context/context-rodrigo/ParqueoProvider";
 import { usePlazas } from "../../context/context-rodrigo/PlazaProvider";
 import { useContratos } from "../../context/context-rodrigo/ContratoProvider";
 function ReportesPage() {
-  const {pagos,loadPagos}=usePagos();
-  const {tarifa2s,loadTarifa2s}=useTarifa2();
-  const {parqueos,loadParqueos}=useParqueos();
-  const {plazas,loadPlazas}=usePlazas();
-  const {contratos,loadContratos}=useContratos();
+  const { pagos, loadPagos } = usePagos();
+  const { tarifa2s, loadTarifa2s } = useTarifa2();
+  const { parqueos, loadParqueos } = useParqueos();
+  const { plazas, loadPlazas } = usePlazas();
+  const { contratos, loadContratos } = useContratos();
 
-  useEffect(()=>{
+  useEffect(() => {
     loadPagos();
     loadTarifa2s();
     loadParqueos();
     loadPlazas();
     loadContratos();
-  },[]);
+  }, []);
   console.log(pagos);
   console.log(tarifa2s);
   console.log(parqueos);
   console.log(plazas);
   console.log(contratos);
-  
-  const getFreeSpaces=()=>{
-    let freeSpaces=0;
+
+  const getFreeSpaces = () => {
+    let freeSpaces = 0;
     for (let i = 0; i < parqueos.length; i++) {
       freeSpaces += parqueos[i].cantidad_sitios;
     }
@@ -36,17 +36,17 @@ function ReportesPage() {
     return freeSpaces;
   }
 
-  const getAssignedSpaces=()=>{
-    let assignedSpaces=0;
+  const getAssignedSpaces = () => {
+    let assignedSpaces = 0;
     let nameParks = [];
     for (let i = 0; i < parqueos.length; i++) {
-      nameParks[i]=parqueos[i].nombre_bloque;
+      nameParks[i] = parqueos[i].nombre_bloque;
     }
     //console.log(nameParks);
     for (let i = 0; i < nameParks.length; i++) {
       let namePark = nameParks[i];
-      for (let j = 0; j < plazas.length; j++){
-        if(namePark===plazas[j].bloque){
+      for (let j = 0; j < plazas.length; j++) {
+        if (namePark === plazas[j].bloque) {
           assignedSpaces++;
         }
       }
@@ -55,91 +55,103 @@ function ReportesPage() {
     return assignedSpaces;
   }
 
-  const getUnAssignedSpaces=()=>{
-    let unAssignedSpaces=0;
-    unAssignedSpaces = getFreeSpaces()-getAssignedSpaces();
+  const getUnAssignedSpaces = () => {
+    let unAssignedSpaces = 0;
+    unAssignedSpaces = getFreeSpaces() - getAssignedSpaces();
     return unAssignedSpaces;
   }
 
-  const totalCollection=()=>{
-    let total=0;
+  const totalCollection = () => {
+    let total = 0;
     for (let i = 0; i < pagos.length; i++) {
       total += pagos[i].monto_pagado;
     }
     return total;
   }
-  const expectedCollection=()=>{
-    let expected=0;
+  const expectedCollection = () => {
+    let expected = 0;
     for (let i = 0; i < pagos.length; i++) {
       for (let j = 0; j < tarifa2s.length; j++) {
         if (pagos[i].tarifa2_id === tarifa2s[j].id) {
-          expected+=tarifa2s[j].costo_tarifa;
+          expected += tarifa2s[j].costo_tarifa;
         }
-        
+
       }
     }
     return expected;
   }
 
-  const getArrears=()=>{
-    let arrears=expectedCollection()-totalCollection();
+  const getArrears = () => {
+    let arrears = expectedCollection() - totalCollection();
     return arrears;
   }
 
-  const getCustomersInArrears=()=>{
-    let customersInArrears=0;
-    let morosos =[];
+  const getCustomersInArrears = () => {
+    let customersInArrears = 0;
+    let morosos = [];
     for (let i = 0; i < contratos.length; i++) {
       let contratoID = contratos[i].id;
-      let cantPagosRealizados = acumularPagos(contratos[i].id);
+      let cantPagosRealizados = acumularPagosPorContrato(contratos[i].id);
       console.log(cantPagosRealizados);
-      let cantPagosEsperados = acumularTarifas(contratos[i].id,contratos[i].created_at);
+      let cantPagosEsperados = acumularTarifasPorContrato(contratos[i].id, contratos[i].created_at);
+      console.log(cantPagosEsperados);
       //console.log(cantPagosEsperados)
       //console.log(cantPagosEsperados);
       //if(cantPagosRealizados<cantPagosEsperados){
-        //morosos.push(contratos[i].id);
-        //console.log(morosos)
+      //morosos.push(contratos[i].id);
+      //console.log(morosos)
       //}
-
+      if (cantPagosRealizados < cantPagosEsperados) {
+        morosos.push(contratos[i]);
+      }
     }
-    return morosos;
-    
+    console.log(morosos);
+    return morosos.length;
+
   }
 
-  const acumularPagos=(contratoID)=>{
+  const acumularPagosPorContrato = (contratoID) => {
     //console.log(contratoID);
     let acumulador = 0;
     for (let j = 0; j < pagos.length; j++) {
-      if (pagos[j].contrato_id === contratoID){
+      if (pagos[j].contrato_id === contratoID) {
         acumulador += pagos[j].monto_pagado;
       }
     }
     return acumulador;
   }
 
-  const acumularTarifas=(contratoID,fecha)=>{
+  const acumularTarifasPorContrato = (contratoID, fecha) => {
     //console.log("la fecha del contrato es: "+fecha);
     let fechaContrato = new Date(fecha);
     //console.log("la fecha del contrato es: "+fechaContrato);
-    let mesContrato = fechaContrato.getMonth()+1;
+    let mesContrato = fechaContrato.getMonth() + 1;
     //console.log("el mes es: "+mesContrato);
     let fechaActual = new Date();
-    let mesActual = fechaActual.getMonth()+1;
-    return mesActual;
-
+    let mesActual = fechaActual.getMonth() + 1;
+    //return mesActual;
+    let acumulador = 0;
+    pagos.forEach(pago => {
+      if (pago.contrato_id===contratoID) {
+        tarifa2s.forEach(tarifa2 => {
+          if (pago.tarifa2_id===tarifa2.id) {
+            acumulador+=tarifa2.costo_tarifa;
+          }
+        });
+      }
+    });
+    return acumulador;
   }
 
-  const selectMonth=()=>{
-    return(
+  const selectMonth = () => {
+    
+    return (
       <select name="selectMonth" id="selectMonth"
-                className="selectMonth"
-              >
-                <option value="1">Enero</option>
-                <option value="2">Febrero</option>
-                <option value="3">Marzo</option>
-                <option value="4">Abril</option>
-                <option value="5">Mayo</option>
-              </select>
+        className="selectMonth"
+      >
+        <option value="1">Abril</option>
+        <option value="2">Mayo</option>
+      </select>
     )
   }
   return (
@@ -168,10 +180,8 @@ function ReportesPage() {
               <select name="selectWeek" id="selectWeek"
                 className="selectWeek"
               >
-                <option value="1">Semana 1</option>
-                <option value="2">Semana 2</option>
-                <option value="3">Semana 3</option>
-                <option value="4">Semana 4</option>
+                <option value="1">Semana 3</option>
+                <option value="2">Semana 4</option>
               </select>
             </div>
             <div
@@ -181,7 +191,7 @@ function ReportesPage() {
                 className="cardRecaudaciones"
               >
                 <h3>Recaudaciones</h3>
-                <p>recaudacion total:</p>
+                <p>recaudacion actual:</p>
                 <p>{totalCollection()} Bs.</p>
                 <p>recaudacion esperada:</p>
                 <p>{expectedCollection()} Bs.</p>
@@ -192,7 +202,11 @@ function ReportesPage() {
                 className="cardClientesEnMora"
               >
                 <h3>Clientes en mora</h3>
+                <div
+                  className="espacioCardClientesEnMora"
+                >
                 <p>{getCustomersInArrears()}</p>
+                </div>
               </div>
               <div
                 className="cardEspaciosDisponibles"
@@ -209,7 +223,6 @@ function ReportesPage() {
           </div>
         </div>
       </div>
-
     </>
   )
 }
