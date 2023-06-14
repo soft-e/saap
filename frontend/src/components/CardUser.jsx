eb9432235f873a32f74663edfeedabd4c7a13c4a
remrodri <rembert.rodrigo@gmail.com>
2023-06-14 18:49:24 -0400

    terminado el card q muestra el tipo de rol del usuario

--- frontend/src/components/CardUser.jsx
+++ frontend/src/components/CardUser.jsx
@@ -2,29 +2,40 @@ import "../assets/css/cardUser.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useEmpleados } from "../context/context-rodrigo/EmpleadoProvider";
 import { useSession } from "../context/context-rodrigo/SessionProvider";
 
 
 function CardUser(){
   const {user}=useSession();
+  
   const {empleados,loadEmpleados}=useEmpleados();
-  console.log(user);
+  //console.log(user);
 
   useEffect(()=>{
     loadEmpleados();
   },[]);
-  console.log(empleados);
+  //console.log(empleados);
 
   const getRolName=()=>{
-    const findedEmployee = empleados.find((value)=>{
-      return value.persona_id === user.id;
-    })
-    console.log(findedEmployee)
+    
+    if(empleados.length>0){
+      const findedEmployee = empleados.find((value)=>{
+        return value.persona_id === user.id;
+      })
+      //console.log(findedEmployee);
+      return !findedEmployee ? "docente":findedEmployee.nombre_cargo; 
+    }
+    
+    //console.log(rol);
+    
+    
     //console.log(employeeFinded.nombre_car
-    const res = findedEmployee.nombre_cargo;
-    return res;
+    //const res = findedEmployee.nombre_cargo;
+
+    //return("rol");
+    //return res;
   }
 
   return <div
@@ -32,7 +43,7 @@ function CardUser(){
   >
     <FontAwesomeIcon icon={faCircleUser} className="icono"/>
     <p className="nombreUsuario">
-      rol
+      {getRolName()}
     </p>
   </div>
 }
